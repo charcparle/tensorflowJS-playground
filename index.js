0a52b60cb51987e02f7b6fa6f2192bacaee0cebf
Charles Cheng <charles.yhcheng@gmail.com>
2021-05-09 22:30:15 +0800

    add and multiply tensors

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ const data2 = tf.variable(tf.zeros([8]));
 
 data.print();
 dataOne.print();
+data2.print();
+
+data2.assign(tf.tensor1d([3,6,7,23,76,68,445,1]))
+data2.print();
+
+// new data sets to try operations
+const data3 = tf.tensor1d([3,4,5,6,7]);
+const data4 = tf.tensor1d([2,3,4,5,10]);
+
+// add and multiply tensors
+data3.add(data4).print();
+data3.mul(data4).print();
