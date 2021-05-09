5b3a4dd3db1ca9ae84e8f05f9d4c65e70ef3942e
Charles Cheng <charles.yhcheng@gmail.com>
2021-05-09 08:36:10 +0800

    created tensor manually

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Show yourself!")
+const shape = [4,2]
+const data = tf.tensor([[4,6],[5,9],[13,25],[1,57]])
+const dataOne = tf.tensor([4,6,5,9,13,25,1,57],shape)
+
+
+// set a variable with zeros method
+const data2 = tf.variable(tf.zeros([8]));
+
+data.print();
+dataOne.print();
